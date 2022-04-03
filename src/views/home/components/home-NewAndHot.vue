83c6c91e64fdeb32ae45600a46fcb3bce5cc3af3
lorea.jin <lorea.jin@71360.com>
2022-04-03 15:45:01 +0800

    feat：面板骨架屏过度效果

--- src/views/home/components/home-NewAndHot.vue
+++ src/views/home/components/home-NewAndHot.vue
@@ -6,35 +6,41 @@
       <template v-slot:right>
         <XtxMore path="/" />
       </template>
-      <ul class="listWrap">
-        <li v-for="(item,i2) in panel.picList()" :key="i2+'pic'">
-          <!-- {{item}}
+
+      <transition name="fade">
+        <ul class="listWrap" v-if="panel.picList().length>0">
+          <li v-for="(item,i2) in panel.picList()" :key="i2+'pic'">
+            <!-- {{item}}
           --------
           {{panel.picList}} -->
-          <RouterLink :to="`/product/${item.id}`" v-if="panel.des==='new'">
-            <img :src="item.picture" alt="">
-            <p class="name ellipsis">{{item.name}}</p>
-            <p class="price">&yen;{{item.price}}</p>
-          </RouterLink>
+            <RouterLink :to="`/product/${item.id}`" v-if="panel.des==='new'">
+              <img :src="item.picture" alt="">
+              <p class="name ellipsis">{{item.name}}</p>
+              <p class="price">&yen;{{item.price}}</p>
+            </RouterLink>
 
-          <RouterLink :to="`/product/${item.id}`" v-else-if="panel.des==='hot'">
-            <img :src="item.picture" alt="">
-            <p class="name">{{item.title}}</p>
-            <p class="desc">{{item.alt}}</p>
-          </RouterLink>
-        </li>
-      </ul>
+            <RouterLink :to="`/product/${item.id}`" v-else-if="panel.des==='hot'">
+              <img :src="item.picture" alt="">
+              <p class="name">{{item.title}}</p>
+              <p class="desc">{{item.alt}}</p>
+            </RouterLink>
+          </li>
+        </ul>
+        <HomeSkeleton v-else />
+      </transition>
+
     </HomePanel>
   </div>
 </template>
 
 <script>
+import HomeSkeleton from '../components/home-skeleton.vue'
 import { findNew, findHot } from '../api/category.js'
 import HomePanel from './home-panel.vue'
 import { ref } from 'vue'
 export default {
   name: 'GoodAndNewIndex',
-  components: { HomePanel },
+  components: { HomePanel, HomeSkeleton },
   props: {},
   setup () {
     let DataArr = ref([])
@@ -93,5 +99,32 @@ export default {
       font-size: 18px;
     }
   }
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.2s;
+}
+
+.fade-enter-to {
+  opacity: 1;
+}
+
+.fade-leave-from {
+  opacity: 1;
+}
+
+.fade-leave-active {
+  transition: all 0.2s;
+  position: absolute;
+  width: 100%;
+  z-index: 1;
+}
+
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
